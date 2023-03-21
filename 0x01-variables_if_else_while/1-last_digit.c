81b158f1f37a472bc3e646a078cdc45403164edf
godlysticksll <godlysticks01@gmail.com>
2023-03-20 17:17:10 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	lastd = n % 10;
 	if (lastd > 5)
 	{
-		printf("lastd digit of %d and is greater than 5\n", lastd);
+		printf("last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
 	else if (lastd == 0)
 	{
