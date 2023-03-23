26372f7a5f607fc8925e0c06fbbd6fca3f661413
godlysticksll <godlysticks01@gmail.com>
2023-03-23 07:33:23 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,13 +12,19 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
-}
-_puchar(48);
-return (0);
-} else if (n < 0)
-{
-	_putchar('-');
-}
-return (-1);
+		{
+			else if (n == 0)
+			{
+				_puchar(48);
+				return (0);
+				{
+					else if (n < 0)
+					{
+						_putchar('-');
+						return (-1);
+					}
+				}
+			}
+		}
+	}
 }
